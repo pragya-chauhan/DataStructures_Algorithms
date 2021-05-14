c91b49311ae6acf49d68b93792e7d67150ffded4
Pragya Chauhan <pragyasinghchauhan13@gmail.com>
2021-05-14 05:02:25 -0400

    Create integer.to.english.cpp

---
+++ integer.to.english.cpp
@@ -0,0 +1,41 @@
+/*
+  ip = int
+  op = string
+  const = nonneg only int number system International
+  
+  ex = 230
+  Two hundred and thirty
+
+  230%10 = 23
+  132%10
+  
+  https://www.youtube.com/watch?v=qwotMTggot0
+
+*/
+
+
+vector<string> twenties = {"", "one", two ,three, four, five, six, seven.............."twenty"};
+
+vector<string> overTwenties = {"", "", "twenty" , "thirty", "fourty"..................."ninety"};
+
+vector<string> thousands = {"", "thousand", "million", "billion"};
+
+string intEng(int num){
+      if(num == 0)
+        return 0;
+  
+      string ans;
+  
+        int index = 0;
+  
+        while(num != 0){
+          
+        
+        
+        
+        }
+
+
+
+
+}
