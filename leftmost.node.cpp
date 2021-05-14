1a3d3d932ad8daa8ff2b18133d0b9de76ec5dd5b
Pragya Chauhan <pragyasinghchauhan13@gmail.com>
2021-05-14 00:37:25 -0400

    Update leftmost.node.cpp

--- leftmost.node.cpp
+++ leftmost.node.cpp
@@ -7,8 +7,25 @@ max 2 children
    8   12   16   25  
 
 ans 15 10 8
-
-vector<int> 
-
-
 */
+vector<int> leftmost(Node* root){
+            if(root == null){
+                   return {};
+            }
+            vector<int> ans;
+            solution(root, &ans);
+
+}
+
+
+void solution(Node* root, vector<int>& ans){
+        if (root==null){
+          return;
+        }
+        ans.push_back(root->value);
+        solution(root->left,ans);
+
+}
+
+
+
