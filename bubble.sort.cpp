8228926b49b25e180198ab313f28214c2b9e559a
Pragya Chauhan <pragyasinghchauhan13@gmail.com>
2021-05-25 11:49:39 -0400

    Create bubble.sort.cpp

---
+++ bubble.sort.cpp
@@ -0,0 +1,3 @@
+/*This is used to identify whether the list is already sorted. When the list is already sorted (which is the best-case scenario), the complexity of bubble sort is only O(n).
+In real life, bubble sort can be visualised when people in a queue wanting to be standing in a height wise sorted manner swap their positions among themselves until everyone is standing based on increasing order of heights.*/
+
