77141aee1f10602637545facbcad5740ed152163
Pragya Chauhan <pragyasinghchauhan13@gmail.com>
2021-05-28 23:23:32 -0400

    Update word.search.cpp

--- word.search.cpp
+++ word.search.cpp
@@ -1,8 +1,34 @@
-
-
-
-bool wordSearch(vector<vector<int>>& board, string& word){
-  if(board == NULL )
+class Solution {
     
+bool search(vector<vector<char>>& board, int i, int j, int count, string& word ){
+  if(count == word.length())
+    return true;
+  if(i<0|| j< 0 || i== board.size()|| j ==board[0].size() || board[i][j] != word[count])
+    return false;
 
+  bool found = false;
+  char temp = board[i][j];
+  board[i][j] = '*';
+
+  found = ( search(board, i-1, j, count+1,  word  )|| search(board, i+1, j, count+1,  word) ||search(board, i, j-1, count+1,  word)|| search(board, i, j+1, count+1,  word));
+  board[i][j] = temp;
+
+return found;
 }
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+         if( board.size() == 0 || board[0].size() == 0 )
+    return false;
+  for(int i = 0 ; i<board.size(); i++){
+    for(int j = 0; j< board[0].size();j++){
+      if (board[i][j] == word[0] && search(board, i, j , 0 , word))
+
+        return true;
+    }
+
+  }
+
+  return false;
+    
+    }
+};
