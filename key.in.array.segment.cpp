cf0250e99250c999f34ee52e04b17ebfd771f49b
Pragya Chauhan <pragyasinghchauhan13@gmail.com>
2021-05-30 20:10:55 -0400

    Update key.in.array.segment.cpp

--- key.in.array.segment.cpp
+++ key.in.array.segment.cpp
@@ -12,11 +12,29 @@ if all true then return true
 
 */
 
-bool findxinkindowSize() {
-  
-
-
+bool findxinkindowSize(int arr[], int x, int k, int n)
+{
+    int i;
+    for (i = 0; i < n; i = i + k) {
+ 
+        // Search x in segment starting
+        // from index i.
+        int j;
+        for (j = 0; j < k; j++){
+          if(j+k < n){
+            if (arr[i + j] == x)
+                break;
+          }
+          else 
+            return false;
+        // If loop didn't break
+        if (j == k)
+           return false;
+        }
+    }
+  return true;
 }
+ 
 
 
 
