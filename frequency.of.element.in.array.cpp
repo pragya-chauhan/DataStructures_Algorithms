3c04446c9c5991ecd4192191471a50a5d046eb9c
Pragya Chauhan <pragyasinghchauhan13@gmail.com>
2021-06-11 06:38:13 -0400

    Update frequency.of.element.in.array.cpp

--- frequency.of.element.in.array.cpp
+++ frequency.of.element.in.array.cpp
@@ -25,11 +25,71 @@ int freq(vector<int> & nums, int tgt){
 1 1 1
 1 9 1
 
+Sorted array
+last occ - first occ +1 = count of elemnets =in sorted array
 */
-int freq(vector<int>&nums, int tgt){
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int last_occ(vector<int>&nums, int tgt){
+  int last = -1;
   
+  int start = 0;
+  int end = nums.size()-1;
+  
+  while(start<=end){
+    int mid = start + (end - start)/2;
+    if(nums[mid] ==  tgt){
+      last = mid;
+      start = mid +1;}
+    else if (nums[mid] > tgt){
+      end = mid-1;
+    }
+    else
+      start = mid +1;
 
+    }
+  return last;
+}
 
+int first_occ(vector<int>&nums, int tgt){
+  int first = -1;
+  
+  int start = 0;
+  int end = nums.size()-1;
+  
+  while(start<=end){
+    int mid = start + (end - start)/2;
+    if(nums[mid] ==  tgt){
+      first = mid;
+      end = mid -1;}
+    else if (nums[mid] > tgt){
+      end = mid-1;
+    }
+    else
+      start = mid +1;
 
+    }
+  return first;
+}
+
+int count(vector<int>&nums, int tgt){
+  int first = first_occ(nums, tgt);
+  int last = last_occ(nums,tgt);
+  if(first!= -1){
+    int count = (last - first) + 1;
+    return count;
+  }
+  else
+    return 0;
+}
+
+int main(){
+    vector<int> nums = {1,2,3,4,4,4,7,8,9,10};
+    int tgt = 4;
+    
+    cout<<count(nums, tgt);
 }
 
