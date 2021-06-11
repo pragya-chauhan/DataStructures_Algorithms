aa42289c6e18eade4bfed414da2639ea46de4eb4
Pragya Chauhan <pragyasinghchauhan13@gmail.com>
2021-06-11 03:06:42 -0400

    Create binary.search.reverse.sorted.array.cpp

---
+++ binary.search.reverse.sorted.array.cpp
@@ -0,0 +1,19 @@
+int reverseBinary(vector<int> input, int tgt){
+  int start = 0;
+  int end = ;nums.size()-1;
+  
+  while(start<=end){
+    int mid = start+ (end - start)/2;
+    
+    if(input[mid] == tgt)
+      return mid;
+    else if(input[mid] > tgt)
+      start = mid +1;
+      else
+        end = mid -1;
+  
+  
+  }
+  return -1;
+
+}
